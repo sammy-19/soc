4465d04284fb0708ec2ece66b495faa2ccac7470
Samson Phiri <sammyhills@gmail.com>
2025-04-13 08:07:03 +0200

    fixed the build_files.sh for vercel deployment

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,10 @@
-pip install -r requirements
-python3.13 manage.py collectstatic
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Run collectstatic (use python3 and add --noinput)
+python3 manage.py collectstatic --noinput
